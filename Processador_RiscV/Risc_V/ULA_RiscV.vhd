671f93e4d1b3eb3c237c7ae4dc017914ff31e35b
Thiago Gomes da Silva Lourenço <thiago.gsl70@gmail.com>
2023-02-22 12:37:08 -0300

    Atualização da ULA, adição de LUI AUIPC e JAL

--- Processador_RiscV/Risc_V/ULA_RiscV.vhd
+++ Processador_RiscV/Risc_V/ULA_RiscV.vhd
@@ -1,51 +1,60 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
+use IEEE.NUMERIC_STD.all;
 
-entity ULA_RiscV is
+entity ULA_RV is
+	generic (WSIZE : natural := 32);
+	port (
+		opcode : in std_logic_vector(3 downto 0);
+		A, B : in std_logic_vector(WSIZE-1 downto 0);
+		Z : out std_logic_vector(WSIZE-1 downto 0));
+end ULA_RV;
 
-generic(WSIZE : natural := 32);
-      
-    
-port( 
-		opcode: in std_logic_vector(3 downto 0);
-  		A, B : in std_logic_vector(WSIZE -1 downto 0);
- 	 	Z : out std_logic_vector(WSIZE -1 downto 0);
-		zero : out std_logic
-);
-end ULA_RiscV;
+architecture behavioral of ULA_RV is
 
-architecture main of ULA_RiscV is
-
---Constantes
-constant ZERO32: std_logic_vector(31 downto 0) := (others => '0');
-constant ONE32: std_logic_vector(31 downto 0) := (0 => '1', others => '0');
-
-signal a32: std_logic_vector(31 downto 0);
-
+	constant ADD_OP : std_logic_vector := "0000";
+	constant SUB_OP : std_logic_vector := "0001";
+	constant AND_OP : std_logic_vector := "0010";
+	constant OR_OP : std_logic_vector := "0011";
+	constant XOR_OP : std_logic_vector := "0100";
+	constant SLL_OP : std_logic_vector := "0101";
+	constant SRL_OP : std_logic_vector := "0110";
+	constant SRA_OP : std_logic_vector := "0111";
+	constant SLT_OP : std_logic_vector := "1000";
+	constant SLTU_OP : std_logic_vector := "1001";
+	constant SGE_OP : std_logic_vector := "1010";
+	constant SGEU_OP : std_logic_vector := "1011";
+	constant SEQ_OP : std_logic_vector := "1100";
+	constant SNE_OP : std_logic_vector := "1101";
+    constant LUI_AUIPC_OP: std_logic_vector := "1110";
+    constant JAL_OP: std_logic_vector := "1111";
+	
+	signal a32 : std_logic_vector(31 downto 0);
+    signal zero : std_logic;
 begin
-  	Z <= a32;
-	proc_ula: process(A, B, opcode, a32) begin
-    
-		if(a32 = ZERO32) then zero <= '1'; else zero <= '0'; end if;
-    
-		case opcode is
-			when "0000" => a32 <= std_logic_vector(signed(A) + signed(B));
-			when "0001" => a32 <= std_logic_vector(signed(A) - signed(B));
-			when "0010" => a32 <= A AND B;
-			when "0011" => a32 <= A OR B;
-			when "0100" => a32 <= A XOR B;
-			when "0101" => a32 <= std_logic_vector(shift_left(unsigned(A),to_integer(unsigned(B))));
-			when "0110" => a32 <= std_logic_vector(shift_right(unsigned(A),to_integer(unsigned(B))));
-			when "0111" => a32 <= std_logic_vector(shift_right(signed(A),to_integer(unsigned(B))));
-			when "1000" => if(signed(A) < signed(B)) then a32 <= ONE32; else a32 <= ZERO32; end if;
-			when "1001" => if(unsigned(A) < unsigned(B)) then a32 <= ONE32; else a32 <= ZERO32; end if;
-			when "1010" => if(signed(A) >= signed(B)) then a32 <= ONE32; else a32 <= ZERO32; end if;
-			when "1011" => if(unsigned(A) >= unsigned(B)) then a32 <= ONE32; else a32 <= ZERO32; end if; 
-			when "1100" => if(A = B) then a32 <= ONE32; else a32 <= ZERO32; end if;
-			when "1101" => if(A /= B) then a32 <= ONE32; else a32 <= ZERO32; end if;
-			when others => a32 <= ZERO32;
-
-		end case;
+	Z <= a32;
+    proc_ula: process (A, B, opcode, a32) begin
+    	if (a32 = x"00000000") then zero <= '1'; else zero <= '0'; end if;
+        case opcode is
+        	when ADD_OP => a32 <= std_logic_vector(signed(A) + signed(B));
+            when SUB_OP => a32 <= std_logic_vector(signed(A) - signed(B));
+            when AND_OP => a32 <= A and B;
+            when OR_OP => a32 <= A or B;
+            when XOR_OP => a32 <= A xor B;
+            when SLL_OP => a32 <= A sll to_integer(unsigned(B));
+            when SRL_OP => a32 <= A srl to_integer(unsigned(B));
+            when SRA_OP => a32 <= std_logic_vector(shift_right(signed(A), to_integer(unsigned(B))));
+            when SLT_OP => a32 <= x"00000001" when A < B else x"00000000";
+            when SLTU_OP => a32 <= x"00000001" when unsigned(A) < unsigned(B) else x"00000000";
+            when SGE_OP => a32 <= x"00000001" when A >= B else x"00000000";
+            when SGEU_OP => a32 <= x"00000001" when unsigned(A) >= unsigned(B) else x"00000000";
+            when SEQ_OP => a32 <= x"00000001" when A = B else x"00000000";
+            when SNE_OP => a32 <= x"00000001" when A /= B else x"00000000";
+            
+            when LUI_AUIPC_OP => a32 <= std_logic_vector(unsigned(A) + shift_left(unsigned(B), 12));
+            when JAL_OP => a32 <= std_logic_vector(unsigned(A) + 4);
+            
+            when others => a32 <= x"00000000";
+        end case;
 	end process;
-end main;
+end;
